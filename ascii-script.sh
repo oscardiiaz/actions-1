aee2714680048fbd0bfe03c63eca617ffc178d27
oscardiiaz <7359173+oscardiiaz@users.noreply.github.com>
2025-05-31 15:18:16 -0700

    modified the ascii script with comment only

--- ascii-script.sh
+++ ascii-script.sh
@@ -4,3 +4,5 @@ cowsay -f dragon "Run for cover, i am a DRAGON....RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
 ls -ltra
+
+# adding a comment to test if workflow will not trigger because of the branch this is in
